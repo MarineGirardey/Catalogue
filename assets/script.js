90071d42d0501ef35a94be9e9a266a4cbceb2e1c
Marine Girardey <marineland24@gmail.com>
2025-05-14 18:57:57 +0200

    Add alt property with a correct description of pictures

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,7 @@ function displayData(json) {
     const element = document.getElementById('products')
     element.innerHTML = json.map(product => `
         <article id=${product.id} class="product-card flex flex-col gap-4 items-start w-full max-w-[280px]" data-type="eyeglasses">
-            <img src="${product.imageUrl}" alt="${product.name}" class="flex w-full pointer-events-auto">
+            <img src="${product.imageUrl}" alt="${product.alt}" class="flex w-full pointer-events-auto">
             <div class="flex flex-col gap-2">
               <div class="flex flex-col items-start">
                 <h3 class="text-lg font-semibold text-black">${product.name}</h3>
