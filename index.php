b95c2e25638d51068767e3347036a85697dc8db6
Marine Girardey <marineland24@gmail.com>
2025-05-14 18:21:14 +0200

    API

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php echo "Bonne chance !"; ?>
+<?php
+header('Content-Type: application/json');
+$json = file_get_contents("products/products.json");
+$data = json_decode($json, true);
+echo json_encode($data);
