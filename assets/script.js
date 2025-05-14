fcb302c564b041fd013a6fc5421e7563f33c6a4a
Marine Girardey <marineland24@gmail.com>
2025-05-14 18:24:37 +0200

    Display products

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,30 @@
 // Fichier pour le code JavaScript frontend
 
+let allProducts = [];
+
 document.addEventListener('DOMContentLoaded', () => {
     fetch('index.php')
     .then(response => response.json())
     .then(data => {
-      displayData(data.products);
+        allProducts = data.products;
+      displayData(allProducts);
       setupFilter();
     })
 });
+
+// Display article dynamically
+function displayData(json) {
+    const element = document.getElementById('products')
+    element.innerHTML = json.map(product => `
+        <article id=${product.id} class="product-card flex flex-col gap-4 items-start w-full max-w-[280px]" data-type="eyeglasses">
+            <img src="${product.imageUrl}" alt="${product.name}" class="flex w-full pointer-events-auto">
+            <div class="flex flex-col gap-2">
+              <div class="flex flex-col items-start">
+                <h3 class="text-lg font-semibold text-black">${product.name}</h3>
+                <p class="text-sm text-black">${product.type}</p>
+              </div>
+              <p class="text-xl font-semibold text-black">${product.price}</p>
+            </div>
+        </article>
+    `).join('');
+}
