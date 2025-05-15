1cc6f6684152dab803b0b94663e6a8428aa52581
Marine Girardey <marineland24@gmail.com>
2025-05-15 22:40:32 +0200

    Move and rename the api to make it cleaner with a proper redirection

--- api/api.php
+++ api/api.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: application/json');
-$json = file_get_contents("products/products.json");
+$json = file_get_contents("./../products/products.json");
 $data = json_decode($json, true);
 echo json_encode($data);
