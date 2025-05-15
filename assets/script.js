2179b2e8051d20f836398ca681362270331c4d0e
Marine Girardey <marineland24@gmail.com>
2025-05-15 22:39:09 +0200

    JS missing function to complete the work

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,19 @@
 // Fichier pour le code JavaScript frontend
-
 let allProducts = [];
+let isModalOpen = false;
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('index.php')
+    fetch('/api/api.php')
     .then(response => response.json())
     .then(data => {
         allProducts = data.products;
-      displayData(allProducts);
-      setupFilter();
+        displayData(allProducts);
+        checkboxTrigger();
+        injectMobileFilter();
+        toggleTrigger();
+        popupTrigger();
+        setupFilterMobile();
+        closeButtonTrigger();
     })
 });
 
@@ -30,23 +35,137 @@ function displayData(json) {
 }
 
 // Checkbox filter
-function setupFilter() {
+function checkboxTrigger() {
 
-    const checkboxes = document.querySelectorAll('.real-checkbox');
+    const filterSidebar = document.getElementById('filter-sidebar');
+    const checkboxes = filterSidebar.querySelectorAll('.real-checkbox');
     
     checkboxes.forEach(checkbox => {
         checkbox.addEventListener('change', () => {
-
-            // Get all checked
-            const selected = Array.from(checkboxes)
-                .filter(checkbox => checkbox.checked)
-                .map(checkbox => checkbox.value);
-
-            // Filter allProduct with types of checked boxes
-            const filtered = selected.length
-                ? allProducts.filter(product => selected.includes(product.type))
-                : allProducts;
-                displayData(filtered);
+            filterProduct(checkboxes);
         });
+    });
+}
+
+// Inject the mobile version of the filter component
+function injectMobileFilter() {
+    // DOM elements
+    const filterSidebarClone = document.getElementById('filter-sidebar').cloneNode(true);
+
+    // Replace IDs to make it unique and easier to manipulare
+    const elementsWithIds = filterSidebarClone.querySelectorAll('[id]');
+    elementsWithIds.forEach((el) => {
+        el.id = el.id + '-mobile';
+    });
+
+    // Small screen elements to add to the filter component
+    const filterFooter = `
+        <div id="separators-mobile" class="flex flex-end border-t border-t-black border-t-solid">
+        <button id="filter-button-mobile" class="mt-3 bg-black text-white px-6 py-2 ml-auto">Filtrer</button>
+        </div>
+    `;
+
+    const CrossButton = `
+        <button id="close-filter-mobile" class="text-black hover:text-red-600 text-2xl leading-none">
+        &times;
+        </button>
+    `;
+
+    // Inject mobile-version filter modale to a dedicated div
+    const filterModal = document.getElementById('filter-modal');
+    filterModal.classList.add('hidden'); // Add hidden class
+    filterModal.innerHTML = filterSidebarClone.innerHTML;
+    filterModal.insertAdjacentHTML('beforeend', filterFooter);
+    // Inject cross button into the header
+    const filterHeader = document.getElementById('close-filter-mobile');
+    filterHeader.innerHTML = CrossButton;
+}
+
+// Checkbox filter - Mobile version
+function setupFilterMobile() {
+
+    // DOM elements
+    const filterSidebar = document.getElementById('filter-modal');
+    const checkboxes = filterSidebar.querySelectorAll('.real-checkbox');
+    const filterButton = document.getElementById('filter-button-mobile');
+
+    filterButton.addEventListener('click', () => {
+        checkboxes.forEach(checkbox => {
+            filterProduct(checkboxes);
+        });
+    });
+}
+
+// Filter product based on selected checkboxes
+function filterProduct(checkboxes) {
+
+    // Get all checked
+    const selected = Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+
+    // Filter allProduct with types of checked boxes
+    const filtered = selected.length
+        ? allProducts.filter(product => selected.includes(product.type))
+        : allProducts;
+        displayData(filtered);
+}
+
+// Type of product toggle
+function toggleTrigger() {
+    // DOM elements
+    const toggle = document.getElementById('accordion-toggle');
+    const content = document.getElementById('accordion-content');
+    const icon = document.getElementById('accordion-icon');
+    // Icon rotation
+    const openedToggle = "rotate(-180deg)";
+    const closedToggle = "rotate(0deg)";
+    // Content visibility
+    const visibleContent = '1';
+    const invisibleContent = '0';
+
+    let isToggleOpen = true;
+    toggle.addEventListener('click', () => {
+        isToggleOpen = !isToggleOpen;
+
+        if(isToggleOpen) {
+            icon.style.transform = closedToggle;
+            content.style.opacity = visibleContent;
+        } else {
+            icon.style.transform = openedToggle;
+            content.style.opacity = invisibleContent;
+        }
+    });
+}
+
+// Filter popup - mobile version - open/close
+function popupTrigger() {
+    // DOM elements
+    const filterButton = document.getElementById('filter-button');
+    const filterModal = document.getElementById('filter-modal');
+
+    filterButton.addEventListener('click', () => {
+        isModalOpen = !isModalOpen;
+        if (isModalOpen) {
+            filterModal.classList.remove('hidden');
+        } else {
+            filterModal.classList.add('hidden');
+        }
+    });
+}
+
+// Cross button managment - mobile version
+function closeButtonTrigger() {
+    // DOM elements
+    const closeModalButton = document.getElementById('close-filter-mobile');
+    const filterModal = document.getElementById('filter-modal');
+
+    closeModalButton.addEventListener('click', () => {
+        isModalOpen = !isModalOpen;
+        if (isModalOpen) {
+            filterModal.classList.remove('hidden');
+        } else {
+            filterModal.classList.add('hidden');
+        }
     });
 }
